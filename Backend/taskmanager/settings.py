2f7b0d22432982dce04cca2083c1f9c5494ad622
BotThunder501 <neisewand@gmail.com>
2025-02-07 14:49:03 +0000

    Moved Secret Key

--- Backend/taskmanager/settings.py
+++ Backend/taskmanager/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0r8y#578#65r+3kqjdd&r@15l0b&f=ffuetgh59dh*t24joew%'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
