758f1885abefc2d56b16c33f6401840208390f5a
Neisewand Hussain <59825874+BotThunder501@users.noreply.github.com>
2025-02-07 14:59:17 +0000

    Delete env.py

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_au7f3wKiGSrm@ep-hidden-thunder-a213ns3s.eu-central-1.aws.neon.tech/heap_grip_aged_605078")
-os.environ.setdefault("SECRET_KEY", "django-insecure-0r8y#578#65r+3kqjdd&r@15l0b&f=ffuetgh59dh*t24joew%")
